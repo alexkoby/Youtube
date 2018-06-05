fd5283cc91e80651bbc5d591960d2c8a3fffd2b1
Alex Koby <alex.r.koby@gmail.com>
2018-06-05 15:07:47 -0400

    Update frontEnd.js

--- frontEnd.js
+++ frontEnd.js
@@ -1,4 +1,19 @@
+console.log("Made it here");
+
+ var pauseButton = document.querySelector(".glyphicon-pause");
+ pauseButton.addEventListener("click", function() {
+     console.log("Clicked pause button");
+     player.pauseVideo();
+ });
+
 var playButton = document.querySelector(".glyphicon-play");
 playButton.addEventListener("click", function(){
 	console.log("Clicked play button");
 });
+
+var submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", function(){
+	var link = document.getElementById("url").innerText;
+	console.log(link);
+});
+
