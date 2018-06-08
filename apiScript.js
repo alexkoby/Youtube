592dab1db0e51b989027e78f6bc4d8339fb982b3
Alex Koby <alex.r.koby@gmail.com>
2018-06-07 22:39:26 -0400

    Starts all videos and pauses all videos when one user clicks pause of start

--- apiScript.js
+++ apiScript.js
@@ -1,65 +1,82 @@
- var player;
- var ss = io();
+includeJS('/socket.io/socket.io.js');
+var socket = io();
+var player;
+//Executes once the youtube API is finished loading
+function onYouTubeIframeAPIReady() {
+    console.log("Inside onYouTubeIframeAPIReady");
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'NYfzmTbb8QY',
+        disablekb: '1',
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
 
- function onYouTubeIframeAPIReady() {
-     console.log("Inside onYouTubeIframeAPIReady");
-     player = new YT.Player('player', {
-         height: '390',
-         width: '640',
-         videoId: 'NYfzmTbb8QY',
-         disablekb: '1',
-         events: {
-             'onReady': onPlayerReady,
-         }
-     });
- }
+//Executes once the player is ready -- after onYoutubeIframeAPIReady()
+function onPlayerReady(event) {
+    console.log("Inside onPlayerReady" + player + " " + event);
+    event.target.playVideo();
+    console.log("Played");
+}
 
- function onPlayerReady(event) {
-     console.log("Inside onPlayerReady" + player + " " + event);
-     
-     event.target.playVideo();
+function onError() {
+    console.log("An error occurred");
+}
 
-     console.log("Played");
- }
+function onPlaybackRateChange() {
+    console.log("OnPlaybackRateChange");
+}
 
- function onError(){
- 	console.log("An error occurred");
- }
+function onStateChange() {
+    console.log("onStateChange");
+}
+//Pause button sends the word 'pause' in the channel 'event' when it's clicked
+var pauseButton = document.querySelector(".glyphicon-pause");
+pauseButton.addEventListener("click", function() {
+    console.log("Clicked pause button");
+    socket.emit('event', 'pause');
+});
+//Pause button sends the word 'play' in the channel 'event' when it's clicked
+var playButton = document.querySelector(".glyphicon-play");
+playButton.addEventListener("click", function() {
+    socket.emit('event', 'play');
+    console.log("Clicked play button");
+});
 
- function onPlaybackRateChange(){
- 	console.log("OnPlaybackRateChange");
- }
-
- function onStateChange(){
- 	console.log("onStateChange");
- }
-
- var pauseButton = document.querySelector(".glyphicon-pause");
- pauseButton.addEventListener("click", function() {
-     console.log("Clicked pause button");
-     player.pauseVideo();
-     console.log("Socket is: " + socket);
-     socket.emit("pause", "Worked");
- });
-
- var playButton = document.querySelector(".glyphicon-play");
- playButton.addEventListener("click", function() {
-     console.log("Clicked play button");
-     player.playVideo();
- });
+//Submit button sends the newVideo code in the 'new link' channel when it's clicked
+var submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", function() {
+    var link = document.getElementById("url").value.toString();
+    for (var i = 0; i < link.length; i++) {
+        if (link.charAt(i) == 'v' && link.charAt(i + 1) == '=') {
+            var newVideo = link.substring(i + 2, i + 13);
+            console.log(newVideo);
+        }
+    }
+    console.log("Link is: " + newVideo);
+    socket.emit('new link', newVideo);
 
 
- var submitButton = document.querySelector("#submit");
- submitButton.addEventListener("click", function() {
-     var link = document.getElementById("url").value.toString();
-     for (var i = 0; i < link.length; i++) {
-         if (link.charAt(i) == 'v' && link.charAt(i + 1) == '=') {
-             var newVideo = link.substring(i + 2, i + 13);
-             console.log(newVideo);
-         }
-     }
-     console.log("Link is: " + link);
+});
 
-     player.cueVideoById(newVideo, 0, "large");
-     player.pauseVideo();
- });
+socket.on('new link', function(msg) {
+    console.log("Received a new link in server");
+    player.cueVideoById(msg, 0, "large");
+    player.pauseVideo();
+});
+
+socket.on('event', function(msg) {
+    if (msg == 'play') {
+        console.log("Event in apiScript inside play");
+        player.playVideo();
+    } else if (msg == 'pause') {
+        player.pauseVideo();
+    }
+})
+
+function includeJS(incFile) {
+    document.write('<script type="text/javascript" src="' + incFile + '"></script>');
+}
