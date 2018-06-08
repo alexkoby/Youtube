f718e804e52ed1ecbd35e1b829cc53af73448dbf
Alex Koby <alex.r.koby@gmail.com>
2018-06-07 22:45:04 -0400

    Websockets that trigger when user hits pause, play, or submits a new video

--- youtubeEmbedded.js
+++ youtubeEmbedded.js
@@ -3,27 +3,41 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+//ensures files are in proper spot
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
 
+//triggers when someone connects to websocket by visiting website
+io.on('connection', function(socket) {
+    console.log('a user connected');
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+    //triggers when someone disconnects from website
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-  socket.on('new link', function(msg){
-  	console.log("New link: " + msg);
-  })
-
+    //Triggers when websocket gets a $msg from the 'new link' channel, which occurs when the user hits the submit button
+    socket.on('new link', function(msg) {
+        //socket.emit('channel', 'msg') -- sends to only person who caused the command
+        //io.emit('channel', 'msg') -- sends too all people connected
+        io.emit('new link', msg);
+        console.log("New link: " + msg);
+    });
+    //Triggers when websocket gets a $msg from the 'event' channel, which occurs when the user hits either the pause or play button
+    socket.on('event', function(msg) {
+        console.log("Event was: " + msg);
+        if (msg == 'play') {
+            console.log("Event inside 'play'")
+            io.emit('event', 'play');
+        } else if (msg == 'pause') {
+            io.emit('event', 'pause');
+        }
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
-    
