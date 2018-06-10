414fcb7acc2a3463c3acb9473cdca25bf1ca0ae0
Alex Koby <alex.r.koby@gmail.com>
2018-06-10 19:18:51 -0400

    Created socket.on('ready') so that new user will go to whatever everyone else is watching

--- youtubeEmbedded.js
+++ youtubeEmbedded.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var videoId = 'NYfzmTbb8QY'
+
 //ensures files are in proper spot
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
@@ -18,9 +20,16 @@ io.on('connection', function(socket) {
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
+    //triggers when new user connects and API is finished loading
+    socket.on('ready', function(msg) {
+    	if(msg == 'ready'){
+    		socket.emit('new link', videoId);
+    	}
+    })
 
     //Triggers when websocket gets a $msg from the 'new link' channel, which occurs when the user hits the submit button
     socket.on('new link', function(msg) {
+    	videoId = msg;
         //socket.emit('channel', 'msg') -- sends to only person who caused the command
         //io.emit('channel', 'msg') -- sends too all people connected
         io.emit('new link', msg);
